3895ac736c1512a24bc0fa3bce922ba320eedc61
André <amorimtorresandre@gmail.com>
2022-03-28 21:35:45 -0300

    main-group-25-HEADER:creating-top-buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Button from '../Button/Button';
 
 function Header() {
   return (
     <div>
-      <h1>testes</h1>
+      <Button
+        className="styles.headerBtn"
+        text="Search"
+        name="profile-btn"
+        data-testid="profile-top-btn"
+        onClick={ () => {} }
+      />
+      <h1 data-testid="page-title"> Foods </h1>
+      <Button
+        className="styles.headerBtn"
+        text="Search"
+        name="search-btn"
+        data-testid="search-top-btn"
+        onClick={ () => {} }
+      />
     </div>
   );
 }
